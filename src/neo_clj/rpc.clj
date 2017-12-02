0a8b11d324a7e0f71fbff15690a8b1dac0dc6f85
Jesse Eisses <jesse.eisses@gmail.com>
2017-12-02 16:02:29 +0100

    Add rpc method for listing assets

--- src/neo_clj/rpc.clj
+++ src/neo_clj/rpc.clj
@@ -1,13 +1,26 @@
 (ns neo-clj.rpc
   (:require
+   [neo-clj.blockchain :as blockchain]
+   [clojure.data.json :as json]
    [clojure.pprint :refer [pprint]]
    [clojure.reflect :refer [reflect]])
   (:import
    Neo.Network.RPC.RpcServer
    Neo.Network.LocalNode
+   Neo.Core.Blockchain
    [Neo.IO.Json JArray JObject]))
 
 (def default-settings {:port 10332 :protocol "http"})
+
+(def method-needs-blockchain? {"getblock" true})
+
+(defn- process-message [settings method params]
+  (if (and (not Blockchain/Default) (method-needs-blockchain? method))
+    {:error "No blockchain loaded"}
+    (case method
+      "getversion" {:port (:port settings) :useragent "NEO-CLJ:0.0.0"}
+      "getassets" (:assets @blockchain/state)
+      nil)))
 
 (defn create-server
   "Initialize an extended RPC server. Can be started with 'start-server'"
@@ -18,9 +31,10 @@
          server
          (proxy [RpcServer] [nil]
            (Process [method params]
-             (case method
-               "getversion" (JObject/Parse (str "{\"port\": " port "}"))
-               (proxy-super Process method params))))]
+             (let [response (process-message settings method params)]
+               (if (nil? response)
+                 (proxy-super Process method params)
+                 (JObject/Parse (json/write-str response))))))]
      (-> settings
          (assoc-in [:server] server)))))
 
