4a6c5d9f6b34f016eba8a642738aa6e97a7ab8fa
Jesse Eisses <jesse.eisses@gmail.com>
2017-12-15 14:01:07 +0100

    Add stub class for ExtendedStateMachine

--- src/neo_clj/implementations.clj
+++ src/neo_clj/implementations.clj
@@ -17,4 +17,36 @@
  :extends Neo.Implementations.Blockchains.LevelDB.LevelDBBlockchain
  :prefix "-")
 
-(defn -OnPersistCompleted ^void [this ^Block block])
+;; (defn -OnPersistCompleted ^void [this ^Block block])
+
+(gen-class
+ :name "neo_clj.implementations.ExtendedStateMachine"
+ :extends Neo.SmartContract.StateMachine
+ :prefix "-SM-"
+ :init init
+ :constructors {[|Neo.IO.Caching.DataCache`2[Neo.UInt160,Neo.Core.AccountState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.Cryptography.ECC.ECPoint,Neo.Core.ValidatorState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.UInt256,Neo.Core.AssetState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.UInt160,Neo.Core.ContractState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.Core.StorageKey,Neo.Core.StorageItem]|]
+                 [|Neo.IO.Caching.DataCache`2[Neo.UInt160,Neo.Core.AccountState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.Cryptography.ECC.ECPoint,Neo.Core.ValidatorState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.UInt256,Neo.Core.AssetState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.UInt160,Neo.Core.ContractState]|
+                 |Neo.IO.Caching.DataCache`2[Neo.Core.StorageKey,Neo.Core.StorageItem]|]}
+ :post-init post-init
+ :methods [[GetCreatedContracts []
+            |System.Collections.Generic.Dictionary`2[Neo.UInt160,Neo.UInt160]|]])
+
+(defn -SM-init [accounts validators assets contracts storages]
+  (println "pre-init")
+  [[accounts validators assets contracts storages] nil])
+
+(defn -SM-post-init [this accounts validators assets contracts storages]
+  (println "post-init")
+  )
+
+(defn -SM-GetCreatedContracts [this]
+  (get-private-field this "contracts_created"))
+
+
